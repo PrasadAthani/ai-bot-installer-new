176a84414f9be3664c0e43b0d4a647fa9657a8d6
PrasadAthani <150219751+PrasadAthani@users.noreply.github.com>
2024-10-07 17:01:07 +0530

    Update 8-iam-oidc.tf

--- AWS/8-iam-oidc.tf
+++ AWS/8-iam-oidc.tf
@@ -1,9 +1,11 @@
-data "tls_certificate" "eks" {
-  url = aws_eks_cluster.[var.name].identity[0].oidc[0].issuer
+# Corrected to remove dynamic resource name
+data "tls_certificate" "eks_oidc" {
+  url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer  # Corrected var.name usage
 }
 
-resource "aws_iam_openid_connect_provider" "eks" {
+# Static resource name and corrected variable interpolation for URL and thumbprint
+resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.[var.name].identity[0].oidc[0].issuer
+  thumbprint_list = [data.tls_certificate.eks_oidc.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer  # Corrected var.name usage
 }
