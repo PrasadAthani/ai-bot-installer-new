ba5c90a5e0e9262a4e4c495cbd68215fbd1111f6
PrasadAthani <150219751+PrasadAthani@users.noreply.github.com>
2024-10-07 16:34:48 +0530

    Update 7-nodes.tf

--- AWS/7-nodes.tf
+++ AWS/7-nodes.tf
@@ -29,7 +29,7 @@ resource "aws_iam_role_policy_attachment" "nodes_amazon_ec2_container_registry_r
 }
 
 resource "aws_eks_node_group" "private_nodes" {
-  cluster_name    = aws_eks_cluster.${var.name}.name
+  cluster_name    = aws_eks_cluster.[var.name].name
   node_group_name = "private-nodes"
   node_role_arn   = aws_iam_role.nodes.arn
 
@@ -67,7 +67,7 @@ resource "aws_eks_node_group" "private_nodes" {
 }
 
 resource "aws_eks_node_group" "large_nodes" {
-  cluster_name    = aws_eks_cluster.${var.name}.name
+  cluster_name    = aws_eks_cluster.[var.name].name
   node_group_name = "large-nodes"
   node_role_arn   = aws_iam_role.nodes.arn
 
