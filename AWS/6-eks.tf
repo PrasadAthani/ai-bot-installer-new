60f7b3c66dbf11eb774f2b835fbbe08844d8d5a0
PrasadAthani <150219751+PrasadAthani@users.noreply.github.com>
2024-10-07 16:17:42 +0530

    Update 6-eks.tf

--- AWS/6-eks.tf
+++ AWS/6-eks.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "sbai" {
+resource "aws_iam_role" "${var.name}" {
   name = "var.name"
 
   assume_role_policy = <<POLICY
@@ -17,14 +17,14 @@ resource "aws_iam_role" "sbai" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "sbai_amazon_eks_cluster_policy" {
+resource "aws_iam_role_policy_attachment" "${var.name}_amazon_eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.sbai.name
+  role       = aws_iam_role.${var.name}.name
 }
 
-resource "aws_eks_cluster" "sbai" {
+resource "aws_eks_cluster" "${var.name}" {
   name     = "var.name"
-  role_arn = aws_iam_role.sbai.arn
+  role_arn = aws_iam_role.${var.name}.arn
   version  = "1.30"
 
   vpc_config {
