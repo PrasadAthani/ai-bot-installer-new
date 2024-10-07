35fbc62a212be2bb81dedcdb29c3c5199412c264
PrasadAthani <150219751+PrasadAthani@users.noreply.github.com>
2024-10-07 16:23:29 +0530

    Update 10-csi-driver-addon.tf

--- AWS/10-csi-driver-addon.tf
+++ AWS/10-csi-driver-addon.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_addon" "csi_driver" {
-  cluster_name             = aws_eks_cluster.${var.name}.name
+  cluster_name             = aws_eks_cluster.[var.name].name
   addon_name               = "aws-ebs-csi-driver"
   addon_version            = "v1.27.0-eksbuild.1"
   service_account_role_arn = aws_iam_role.eks_ebs_csi_driver.arn
